b1cd9826cd900a3a7f720c81245d7cbf41bc8049
omkottewar <omkottewar19.04@gmail.com>
2026-03-27 20:20:52 +0530

    CHANGED DB CODE

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,17 +1,6 @@
-import pg from 'pg';
-import { config } from '../config/index.js';
-
-const { Pool } = pg;
+import pkg from 'pg';
+const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: config.databaseUrl,
-  max: 20,
-  idleTimeoutMillis: 30000,
-ssl: {
-  rejectUnauthorized: false,
-  require: true,
-},
+  connectionString: process.env.DATABASE_URL,
 });
-pool.connect()
-  .then(() => console.log("✅ DB CONNECTED"))
-  .catch((err) => console.error("❌ DB CONNECTION ERROR:", err));
