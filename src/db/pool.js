5badf745c0df9584a191cd89a4b354aa6048cb06
omkottewar <omkottewar19.04@gmail.com>
2026-03-27 17:30:17 +0530

    ALERT

--- src/db/pool.js
+++ src/db/pool.js
@@ -7,4 +7,7 @@ export const pool = new Pool({
   connectionString: config.databaseUrl,
   max: 20,
   idleTimeoutMillis: 30000,
+    ssl: {
+    rejectUnauthorized: false, // 🔥 THIS FIXES YOUR ERROR
+  },
 });
