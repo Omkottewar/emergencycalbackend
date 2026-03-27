d4d65ede775a4b2d883a7399a8d369df06098de0
omkottewar <omkottewar19.04@gmail.com>
2026-03-27 17:35:42 +0530

    ssl update

--- src/db/pool.js
+++ src/db/pool.js
@@ -7,7 +7,8 @@ export const pool = new Pool({
   connectionString: config.databaseUrl,
   max: 20,
   idleTimeoutMillis: 30000,
-    ssl: {
-    rejectUnauthorized: false, // 🔥 THIS FIXES YOUR ERROR
-  },
+ssl: {
+  rejectUnauthorized: false,
+  require: true,
+},
 });
